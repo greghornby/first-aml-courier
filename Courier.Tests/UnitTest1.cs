f8785c3a098e3f45850739c1e943e6709f3b6ff9
greg <greg.hornby.314@gmail.com>
2025-11-10 18:03:48 +1300

    feat(Weight Limit):
    - Refactor some switch statements to own methods
    - Add a method to get the WeightLimit of a parcel
    - If a parcel is overweight, incur an additional charge of $2 for every kg over the limit
    - Do not affect base parcel cost, but track it on each parcel as a separate field, also track the total overweight charge
    - Update TotalCost to include overweight charge

--- Courier.Tests/UnitTest1.cs
+++ Courier.Tests/UnitTest1.cs
@@ -6,14 +6,16 @@ public class UnitTest1
     [TestMethod]
     public void TestCreateOrder_NoSpeedy()
     {
+
+        // lazily set weight to 0 as we're uninterested in this atm
         var order = CourierService.CreateOrder([
-            new CourierService.OrderInputPackage { Size = 5 },
-            new CourierService.OrderInputPackage { Size = 20 },
-            new CourierService.OrderInputPackage { Size = 80 },
-            new CourierService.OrderInputPackage { Size = 120 }
+            new CourierService.OrderInputPackage { Size = 5, Weight = 0 },
+            new CourierService.OrderInputPackage { Size = 20, Weight = 0 },
+            new CourierService.OrderInputPackage { Size = 80, Weight = 0 },
+            new CourierService.OrderInputPackage { Size = 120, Weight = 0 }
         ], new CourierService.OrderInputOptions { SpeedyDelivery = false });
 
-        Assert.AreEqual(51, order.PackageTotalCost); // package total should be $51
+        Assert.AreEqual(51, order.PackageTotalBaseCost); // package total should be $51
         Assert.AreEqual(false, order.SpeedyDelivery); // no speedy
         Assert.AreEqual(0, order.SpeedyCost); // no speedy, no extra cost!
         Assert.AreEqual(51, order.TotalCost); // therefore total order should be $51
@@ -41,7 +43,7 @@ public class UnitTest1
             new CourierService.OrderInputPackage { Size = 120 }
         ], new CourierService.OrderInputOptions { SpeedyDelivery = true });
 
-        Assert.AreEqual(51, order1.PackageTotalCost); // package total should be $51
+        Assert.AreEqual(51, order1.PackageTotalBaseCost); // package total should be $51
         Assert.AreEqual(true, order1.SpeedyDelivery); // speedy should be anbled
         Assert.AreEqual(51, order1.SpeedyCost); // speedy costs should match package cost
         Assert.AreEqual(102, order1.TotalCost); // therefore total order should be $102 ($51 base cost + $51 speedy cost)
@@ -56,6 +58,41 @@ public class UnitTest1
                 new CourierService.OrderSummaryPackage { Size = 120, Cost = 25, SizeLabel = CourierService.PackageSizeLabel.XL }
             },
             order1.Packages.ToArray()
+        );
+    }
+
+    [TestMethod]
+    public void TestCreateOrder_Overweight()
+    {
+        var order = CourierService.CreateOrder([
+            new CourierService.OrderInputPackage { Size = 5, Weight = 1 }, //not overweight, actually on the exact weight limit
+            new CourierService.OrderInputPackage { Size = 20, Weight = 6 }, //3kg overweight should incur an additional cost of $6
+            new CourierService.OrderInputPackage { Size = 80, Weight = 4 }, //not overweight, under the limit
+            new CourierService.OrderInputPackage { Size = 120, Weight = 11 } //1kg overweight, should incur $2
+        ], new CourierService.OrderInputOptions { SpeedyDelivery = false });
+
+        // check with dotnet test --logger "console;verbosity=detailed"
+        // Console.WriteLine(order);
+
+        int expectedPackageBase = 51;
+        int expectedOverweightTotal = 8; // from the 2nd and 4th packages
+
+        Assert.AreEqual(expectedPackageBase, order.PackageTotalBaseCost);
+        Assert.AreEqual(false, order.SpeedyDelivery);
+        Assert.AreEqual(0, order.SpeedyCost);
+        Assert.AreEqual(expectedOverweightTotal, order.OverWeightTotalCost);
+        Assert.AreEqual(expectedPackageBase + expectedOverweightTotal, order.TotalCost); //should be base packages + overweight (no speedy)
+
+        // check the packages got size labelled correctly
+        CollectionAssert.AreEqual(
+            new[]
+            {
+                new CourierService.OrderSummaryPackage { Size = 5, Cost = 3, SizeLabel = CourierService.PackageSizeLabel.S, OverWeightAmount = 0, OverweightCharge = 0 },
+                new CourierService.OrderSummaryPackage { Size = 20, Cost = 8, SizeLabel = CourierService.PackageSizeLabel.M, OverWeightAmount = 3, OverweightCharge = 6 },
+                new CourierService.OrderSummaryPackage { Size = 80, Cost = 15, SizeLabel = CourierService.PackageSizeLabel.L, OverWeightAmount = 0, OverweightCharge = 0 },
+                new CourierService.OrderSummaryPackage { Size = 120, Cost = 25, SizeLabel = CourierService.PackageSizeLabel.XL, OverWeightAmount = 1, OverweightCharge = 2 }
+            },
+            order.Packages.ToArray()
         );
     }
 }
--- Courier/Courier.cs
+++ Courier/Courier.cs
@@ -4,43 +4,88 @@ namespace Courier;
 
 public class CourierService
 {
+    static private int OVERWEIGHT_PER_KG_CHARGE = 2;
+
     static public OrderSummary CreateOrder(IEnumerable<OrderInputPackage> packages, OrderInputOptions options)
     {
         int packageTotalCostInDollars = 0;
+        int totalOverweightCost = 0;
         var summaryPackages = new List<OrderSummaryPackage>();
 
         foreach (OrderInputPackage package in packages)
         {
-            int costInDollars = package.Size switch
+            int costInDollars = GetBaseCostForPackage(package);
+            PackageSizeLabel sizeLabel = GetSizeLabelForPackage(package);
+            int weightLimit = GetWeightLimitForPackage(package, sizeLabel);
+            int overWeightAmount = 0;
+            int overWeightCharge = 0;
+            if (package.Weight > weightLimit)
             {
-                var s when s < 10 => 3,
-                var s when s < 50 => 8,
-                var s when s < 100 => 15,
-                _ => 25
-            };
-            PackageSizeLabel sizelabel = package.Size switch
-            {
-                var s when s < 10 => PackageSizeLabel.S,
-                var s when s < 50 => PackageSizeLabel.M,
-                var s when s < 100 => PackageSizeLabel.L,
-                _ => PackageSizeLabel.XL
-            };
-            summaryPackages.Add(new OrderSummaryPackage { Size = package.Size, Cost = costInDollars, SizeLabel = sizelabel });
+                // assuming weights are rounded to integers (up, down or closest, who knows!)
+                int weightDiff = package.Weight - weightLimit;
+                overWeightAmount = weightDiff;
+                overWeightCharge = weightDiff * OVERWEIGHT_PER_KG_CHARGE;
+                totalOverweightCost += overWeightCharge;
+            }
+            summaryPackages.Add(new OrderSummaryPackage { Size = package.Size, Cost = costInDollars, SizeLabel = sizeLabel, OverWeightAmount = overWeightAmount, OverweightCharge = overWeightCharge });
             packageTotalCostInDollars += costInDollars;
         }
 
         bool isSpeedy = options.SpeedyDelivery;
-        int totalCostInDollars = packageTotalCostInDollars * (isSpeedy ? 2 : 1);
+        int totalCostInDollars = packageTotalCostInDollars * (isSpeedy ? 2 : 1) + totalOverweightCost;
         int speedyCost = isSpeedy ? packageTotalCostInDollars : 0;
 
-        return new OrderSummary { TotalCost = totalCostInDollars, Packages = summaryPackages, PackageTotalCost = packageTotalCostInDollars, SpeedyDelivery = isSpeedy, SpeedyCost = speedyCost  };
+        return new OrderSummary { TotalCost = totalCostInDollars, Packages = summaryPackages, PackageTotalBaseCost = packageTotalCostInDollars, OverWeightTotalCost = totalOverweightCost, SpeedyDelivery = isSpeedy, SpeedyCost = speedyCost };
+    }
+
+    static private int GetBaseCostForPackage(OrderInputPackage package)
+    {
+        int costInDollars = package.Size switch
+        {
+            var s when s < 10 => 3,
+            var s when s < 50 => 8,
+            var s when s < 100 => 15,
+            _ => 25
+        };
+
+        return costInDollars;
+    }
+
+    static private PackageSizeLabel GetSizeLabelForPackage(OrderInputPackage package)
+    {
+        PackageSizeLabel sizelabel = package.Size switch
+        {
+            var s when s < 10 => PackageSizeLabel.S,
+            var s when s < 50 => PackageSizeLabel.M,
+            var s when s < 100 => PackageSizeLabel.L,
+            _ => PackageSizeLabel.XL
+        };
+
+        return sizelabel;
+    }
+
+    // Can provide the sizeLabel if it is already known, otherwise it will be calculated
+    static private int GetWeightLimitForPackage(OrderInputPackage package, PackageSizeLabel? sizeLabel)
+    {
+        var calculatedSizeLabel = sizeLabel ?? GetSizeLabelForPackage(package);
+
+        int weightLimit = calculatedSizeLabel switch
+        {
+            PackageSizeLabel.S => 1,
+            PackageSizeLabel.M => 3,
+            PackageSizeLabel.L => 6,
+            PackageSizeLabel.XL => 10,
+            _ => 10 //for some reason it said the switch cases were non-exhaustive (even though all the enum values are covered), so add the default case (might take too much time to investigate)
+        };
+
+        return weightLimit;
     }
 
 
 
-    public enum PackageSizeLabel { S, M, L, XL};
-    public readonly record struct OrderInputPackage(int Size);
+    public enum PackageSizeLabel { S, M, L, XL };
+    public readonly record struct OrderInputPackage(int Size, int Weight);
     public readonly record struct OrderInputOptions(bool SpeedyDelivery);
-    public readonly record struct OrderSummaryPackage(int Size, int Cost, PackageSizeLabel SizeLabel);
-    public readonly record struct OrderSummary(IReadOnlyList<OrderSummaryPackage> Packages, int PackageTotalCost, bool SpeedyDelivery, int? SpeedyCost, int TotalCost);
+    public readonly record struct OrderSummaryPackage(int Size, int Cost, PackageSizeLabel SizeLabel, int OverWeightAmount, int OverweightCharge);
+    public readonly record struct OrderSummary(IReadOnlyList<OrderSummaryPackage> Packages, int PackageTotalBaseCost, int OverWeightTotalCost, bool SpeedyDelivery, int? SpeedyCost, int TotalCost);
 }
