e085c7d22a8561a26c886f47140eb2ace26170c8
greg <greg.hornby.314@gmail.com>
2025-11-10 17:12:24 +1300

    feat(CreateOrder):
    - Add a CreateOrder method to CourierService
    -- This accepts a list of packages (currently just with a Size property)
    -- Returns a summary of total cost, plus a list of the packages mapped to their original size, a size label, and cost

--- Courier.Tests/UnitTest1.cs
+++ Courier.Tests/UnitTest1.cs
@@ -3,9 +3,26 @@ namespace Courier.Tests;
 [TestClass]
 public class UnitTest1
 {
-    [TestMethod()]
-    public void TestMethod1()
+    [TestMethod]
+    public void TestCreateOrder()
     {
-        Assert.AreEqual(5, CourierService.Test());
+        var order1 = CourierService.CreateOrder([
+            new CourierService.OrderInputPackage { Size = 5 },
+            new CourierService.OrderInputPackage { Size = 20 },
+            new CourierService.OrderInputPackage { Size = 80 },
+            new CourierService.OrderInputPackage { Size = 120 }
+        ]);
+
+        Assert.AreEqual(51, order1.TotalCost);
+        CollectionAssert.AreEqual(
+            new[]
+            {
+                new CourierService.OrderSummaryPackage { Size = 5, Cost = 3, SizeLabel = CourierService.PackageSizeLabel.S },
+                new CourierService.OrderSummaryPackage { Size = 20, Cost = 8, SizeLabel = CourierService.PackageSizeLabel.M },
+                new CourierService.OrderSummaryPackage { Size = 80, Cost = 15, SizeLabel = CourierService.PackageSizeLabel.L },
+                new CourierService.OrderSummaryPackage { Size = 120, Cost = 25, SizeLabel = CourierService.PackageSizeLabel.XL }
+            },
+            order1.Packages.ToArray()
+        );
     }
 }
--- Courier/Courier.cs
+++ Courier/Courier.cs
@@ -1,9 +1,41 @@
-﻿namespace Courier;
+﻿using System.Linq;
+
+namespace Courier;
 
 public class CourierService
 {
-    static public int Test()
+    static public OrderSummary CreateOrder(IEnumerable<OrderInputPackage> packages)
     {
-        return 5;
+        int totalCostInDollars = 0;
+        var summaryPackages = new List<OrderSummaryPackage>();
+
+        foreach (OrderInputPackage package in packages)
+        {
+            int costInDollars = package.Size switch
+            {
+                var s when s < 10 => 3,
+                var s when s < 50 => 8,
+                var s when s < 100 => 15,
+                _ => 25
+            };
+            PackageSizeLabel sizelabel = package.Size switch
+            {
+                var s when s < 10 => PackageSizeLabel.S,
+                var s when s < 50 => PackageSizeLabel.M,
+                var s when s < 100 => PackageSizeLabel.L,
+                _ => PackageSizeLabel.XL
+            };
+            summaryPackages.Add(new OrderSummaryPackage { Size = package.Size, Cost = costInDollars, SizeLabel = sizelabel });
+            totalCostInDollars += costInDollars;
+        }
+
+        return new OrderSummary { TotalCost = totalCostInDollars, Packages = summaryPackages };
     }
+
+
+
+    public enum PackageSizeLabel { S, M, L, XL};
+    public readonly record struct OrderInputPackage(int Size);
+    public readonly record struct OrderSummaryPackage(int Size, int Cost, PackageSizeLabel SizeLabel);
+    public readonly record struct OrderSummary(IReadOnlyList<OrderSummaryPackage> Packages, int TotalCost);
 }
